e013792ff6950ecc8fe0375f75815adc11008d6a
Gerardo <gm547@cornell.edu>
2024-02-01 20:01:38 -0500

    Added frontend and backend for meal planner

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/home", (req, res) => {
 })
 
 
-app.post("/mealplan", async (req, res) => {
+app.post("/unknown", async (req, res) => {
   const { messages } = req.body;
 
   const chatCompletion = await openai.chat.completions.create({
@@ -46,14 +46,41 @@ app.post("/mealplan", async (req, res) => {
   })
 });
 
+
+app.post("/mealplan", async (req, res) => {
+  const { messages } = req.body;
+
+
+  const gptConfiguration = "You will receive a profile for a person that includes" +
+    "their age, gender, height, weight, excercise frequency, and goal. Your job is to " +
+    "create a nutrition plan. With the given information, you will return a plan in json format as follows:" +
+    "{ 'breakfast': 'breakfast plan here', 'lunch' : 'lunch plan here', 'dinner': 'dinner plan here'} " +
+    "Please ensure that you only return a json of the described format. Insert each individual plan into the correct place in the json"
+
+
+  const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { "role": "system", "content": gptConfiguration },
+      ...messages
+
+    ],
+  })
+
+  res.json({
+    completion: chatCompletion.choices[0].message
+  })
+});
+
+
+
 app.post("/calories", async (req, res) => {
   const { messages } = req.body;
 
   const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
-      { "role": "system", "content":  "You are a calorie counter. Your responses are limited to just a number. You'll be given lists of food items without calorie counts. Assume one item per entry. If count of an item is confusing, assume just one of said item. Calculate and return the total calories. I want to emphasize again that you must only return just a number. Do not return any words before or after the number." },
-      ...messages
+      { "role": "system", "content": "You are a calorie counter. Your responses are limited to just a number. You'll be given lists of food items without calorie counts. Assume one item per entry. If count of an item is confusing, assume just one of said item. Calculate and return the total calories. I want to emphasize again that you must only return just a number. Do not return any words before or after the number." },
     ],
 
   });
