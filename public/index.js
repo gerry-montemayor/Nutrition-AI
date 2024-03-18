fc59d4b5770edd261f881a15a934bb36621dba3a
Gerardo <gm547@cornell.edu>
2024-03-18 00:08:53 -0400

    fixes

--- public/index.js
+++ public/index.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { chatGPTApiKey, foodDataApiKey } from "../config.js"
-import axios from "axios";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -37,7 +36,6 @@ app.post("/unknown", async (req, res) => {
   const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
-      // { role: "user", content: `${message}` },
       { "role": "system", "content": "You are NutritionGPT, a helpful nutrition advice assistant chatbot." },
       ...messages
     ],
